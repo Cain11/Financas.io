44ca67b6601b8a8225355e092f5cfa18305a0384
Caio Torres <caiotorres99@outlook.com>
2024-05-27 20:32:28 -0400

    Add project files.

---
+++ Fina.Core/Models/Transaction.cs
@@ -0,0 +1,18 @@
+﻿using Fina.Core.Enums;
+
+namespace Fina.Core.Models
+{
+    public class Transaction
+    {
+        public long Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? PaidOrReceivedAt { get; set; }
+
+        public ETransactionType Type { get; set; } = ETransactionType.Withdrawal;
+        public Category Category { get; set; }
+
+        public string UserId { get; set; }
+    }
+}
