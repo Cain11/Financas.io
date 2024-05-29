77e4aff4dc864e288905ce348f5341db40120e0b
Caio Torres <caiotorres99@outlook.com>
2024-05-29 02:53:25 -0400

    Finalizando dia 1

---
+++ Fina.Core/Handlers/ITransactionHandler.cs
@@ -0,0 +1,15 @@
+﻿using Fina.Core.Requests.Transactions;
+using Fina.Core.Responses;
+using System.Transactions;
+
+namespace Fina.Core.Handlers
+{
+    public interface ITransactionHandler
+    {
+        Task<Response<Transaction?>> CreateAsync(CreateTransactionRequest createTransactionRequest);
+        Task<Response<Transaction?>> UpdateAsync(UpdateTransactionRequest updateTransactionRequest);
+        Task<Response<Transaction?>> DeleteAsync(DeleteTransactionRequest deleteTransactionRequest);
+        Task<Response<Transaction?>> GetByIdAsync(GetTransactionByIdRequest getTransactionRequest);
+        Task<PagedResponse<List<Transaction>?>> GetByPeriodAsync(GetTransactionByPeriodRequest getTransactionByPeriodRequest);
+    }
+}
--- Fina.Core/Models/Transaction.cs
+++ Fina.Core/Models/Transaction.cs
@@ -11,6 +11,8 @@ namespace Fina.Core.Models
         public DateTime? PaidOrReceivedAt { get; set; }
 
         public ETransactionType Type { get; set; } = ETransactionType.Withdrawal;
+        public decimal Amount { get; set; }
+
         public Category Category { get; set; }
 
         public string UserId { get; set; }
